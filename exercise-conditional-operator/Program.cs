dc013930c3bc2d930ea8b577dc46654fd1fd34ae
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2024-04-23 14:40:48 -0400

    Evaluate boolean expression complete

---
+++ exercise-conditional-operator/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace exercise_conditional_operator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // int saleAmount = 1001;
+        // System.Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+        // CHALLENGE 1 - flip coin
+        // recreate the flip of a coin
+        // Random coin = new Random();
+        // int flip = coin.Next(0, 2);
+        // System.Console.WriteLine(flip == 0 ? "Heads" : "Tails");
+
+        // CHALLENGE 2 - DECISION LOGIC
+        string permission = "Admin|Manager";
+        int level = 19;
+
+        if (permission.Contains("Admin"))
+        {
+            if (level > 55)
+                Console.WriteLine("Welcome, Super Admin user.");
+            else
+                Console.WriteLine("Welcome, Admin user.");
+        }
+        else if (permission.Contains("Manager"))
+        {
+            if (level > 20)
+                Console.WriteLine("Contact an Admin for access.");
+            else
+                Console.WriteLine("You do not have sufficient privileges.");
+        }
+        else
+            Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
