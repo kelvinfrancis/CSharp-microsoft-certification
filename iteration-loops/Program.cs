1130fc25952c2c448389f2742be20ddaa42aeb5a
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2024-04-24 17:49:32 -0400

    FizzBuzz challenge complete

---
+++ iteration-loops/Program.cs
@@ -0,0 +1,23 @@
+﻿// string [] names = { "Alex", "Eddie", "David", "Michael" };
+// for (int i = names.Length - 1; i >= 0; i--)
+// {
+//     if (names[i] == "David") names[i] = "Sammy";
+// }
+
+// foreach (var name in names)
+// {
+//     System.Console.WriteLine(name);
+// }
+
+// FizzBuzz Challenge
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 5 == 0 && i % 3 == 0)
+        System.Console.WriteLine($"{i} - FizzBuzz");
+    else if (i % 3 == 0)
+        System.Console.WriteLine($"{i} - Fizz");
+    else if (i % 5 == 0)
+        System.Console.WriteLine($"{i} - Buzz");
+    else
+        System.Console.WriteLine(i);
+}
