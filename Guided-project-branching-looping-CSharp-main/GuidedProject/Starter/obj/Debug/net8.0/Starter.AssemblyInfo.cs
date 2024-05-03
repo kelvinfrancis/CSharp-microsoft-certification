ba929d0e51c98f310642353985514041f08de1f2
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2024-05-03 16:42:32 +0000

    Menu: part 1 and 2 complete

--- Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1130fc25952c2c448389f2742be20ddaa42aeb5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb12e0cfd3574d1d10bf7677f0e43c8b446c0c7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
