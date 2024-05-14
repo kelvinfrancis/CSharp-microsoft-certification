7da2633e0b35fce66f5ebabbba22136c2600189a
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2024-05-14 16:14:29 -0400

    modulo complete

---
+++ uso-metodo-indexOf-Substring/Program.cs
@@ -0,0 +1,116 @@
+﻿// -------------- IndexOf() and Substring()
+/*
+string message = "What is the value <span>between the tags</span>?";
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openingPosition = message.IndexOf(openSpan);
+int closingPosition = message.IndexOf(closeSpan);
+
+openingPosition += openSpan.Length;
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
+*/
+
+// --------------- IndexOfAny() and LastIndexOf()
+/*
+string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+while (true)
+{
+    int openingPosition = message.IndexOf("(");
+    if (openingPosition == -1) break;
+
+    openingPosition += 1;
+    int closingPosition = message.IndexOf(')');
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+
+    message = message.Substring(closingPosition + 1);
+}
+
+string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+// The IndexOfAny() helper method requires a char array of characters. 
+// You want to look for:
+
+char[] openSymbols = {'[', '{', '('};
+
+// You'll use a slightly different technique for iterating through 
+// the characters in the string. This time, use the closing 
+// position of the previous iteration as the starting index for the 
+//next open symbol. So, you need to initialize the closingPosition 
+// variable to zero:
+int closingPosition = 0;
+
+while (true)
+{
+    int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
+    if (openingPosition == -1) break;
+
+    string currentSymbol = message.Substring(openingPosition, 1);
+
+    // Now find the matching closing symbol
+    char matchingSymbol = ' ';
+
+    switch (currentSymbol)
+    {
+        case "[":
+            matchingSymbol = ']';
+            break;
+        case "{":
+            matchingSymbol = '}';
+            break;
+        case "(":
+            matchingSymbol = ')';
+            break;
+    }
+
+    // To find the closingPosition, use an overload of the IndexOf method to specify 
+    // that the search for the matchingSymbol should start at the openingPosition in the string. 
+
+    openingPosition += 1;
+    closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+        // Finally, use the techniques you've already learned to display the sub-string:
+
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+}
+*/
+
+/* ------- Remove() y Replace()
+string data = "12345John Smith          5000  3  ";
+string updatedData = data.Remove(5, 20);
+Console.WriteLine(updatedData);
+
+// Replace()
+string message = "This--is--ex-amp-le--da-ta";
+message = message.Replace("--", " ");
+message = message.Replace("-", "");
+Console.WriteLine(message);
+*/
+
+/* Desafío para extraer, reemplazar y quitar datos de una cadena de entrada */
+
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openingSpanPosition = input.IndexOf(openSpan);
+int closingSpanPosition = input.IndexOf(closeSpan);
+
+openingSpanPosition += openSpan.Length;
+int length = closingSpanPosition - openingSpanPosition;
+quantity = input.Substring(openingSpanPosition, length);
+
+output = input.Remove(0,5).Replace("</div>", "").Replace("&trade;", "&reg;");
+
+Console.WriteLine("Quality: " + quantity);
+Console.WriteLine("Output: "+ output);
